3925523832acaa6fd091fa7b8506879c90f9c3f3
Patryk Kuciński <kucinskipatryk@icloud.com>
2022-06-24 21:53:20 +0200

    First commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn escobar.wsgi --log-file -
--- escobar/settings.py
+++ escobar/settings.py
@@ -10,22 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^se@#mpqu766cg*_6f1jdty&q4fu()y-u=fmn+z7-ed4%k_7n8'
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['escobar-olsztynek.herokuapp.com', 'localhost:8000', 'escobar-olsztynek.pl', '127.0.0.1:8000']
 
 
 # Application definition
@@ -72,15 +71,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'escobar.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
 # Password validation
@@ -132,10 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = ['static']
+
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = 'media'
+
 USER_AGENTS_CACHE = 'default'
 
 

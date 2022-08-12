d2749d9d0a2b940afddf2615e62f8790415bc08e
pkucinski <Cicinho94>
2022-08-12 21:56:16 +0200

    add open hours

--- escobar/urls.py
+++ escobar/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import index, new_position, edit, delete, edit_hours
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -43,4 +45,4 @@ urlpatterns = [
     path('usun_kieliszek/<int:id>/', delete),
 
     path('edycja_godzin_otwarcia/<int:id>/', edit_hours),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

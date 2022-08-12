d14c51f060323251e62163749dc8a71388dadced
pkucinski <Cicinho94>
2022-08-12 22:03:07 +0200

    add open hours

--- escobar/settings.py
+++ escobar/settings.py
@@ -123,12 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 #Przy deplopy na heroku wylaczyc
-S1TATICFILES_DIRS = ['static']
+STATICFILES_DIRS = ['static']
 
 MEDIA_URL = '/media/'
 
